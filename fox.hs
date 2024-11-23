f417a477f21b19eda1e673c371c1d3cf6806942c
Jason Manuel <jama.indo@hotmail.com>
2024-11-23 01:04:14 -0700

    Generate combinations of positions as bit strings instead of lists

--- fox.hs
+++ fox.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE BinaryLiterals #-}
 
 import Data.Bits
+import Data.Char
 import Data.List
 import Data.Maybe
 import Data.Monoid
 import Data.Traversable
 import Data.Word
+import Numeric
 
 choose n k = product [n - k + 1 .. n] `quot` product [1 .. k]
 
@@ -34,18 +36,29 @@ Representation:
 -}
 
 cell n board = shiftR board (shiftL n 1) .&. 0b11
+
 setCell n letter board = shiftL letter (shiftL n 1) .|. board
 
 -- QUESTION: Can I generate boards just from an integer?
 combos :: Int -> [a] -> [[a]]
 combos 0 options = [[]]
 combos k options =
-  concatMap go (tails options) where
+  concatMap go (tails options)
+  where
     go :: [a] -> [[a]]
     go os =
       case os of
         (o : os) -> (o :) <$> combos (k - 1) os
         [] -> []
+
+-- https://www.baeldung.com/cs/generate-k-combinations#2-revolving-door-algorithm
+grayCombos :: Int -> Int -> [Word16]
+grayCombos n k = go n (n - k) k
+  where
+    go :: Int -> Int -> Int -> [Word16]
+    go _ _ 0 = [0]
+    go 0 _ _ = []
+    go n nMinusK k = go (n - 1) (nMinusK - 1) k ++ ((.|. shiftL 1 (n - 1)) <$> reverse (go (n - 1) nMinusK (k - 1)))
 
 positions = [0 .. 15]
 
@@ -61,6 +74,17 @@ placeXs poss board = do
   poss <- combos xs poss
   pure $ foldr (\p b -> setCell p 0b10 b) board poss
 
+placeAll fPoss oPoss board = go fPoss oPoss 0 board
+  where
+    go _ _ 16 board = board
+    go fPoss oPoss curPos board =
+      if fPoss .&. 1 == 1
+        then go (shiftR fPoss 1) oPoss (curPos + 1) (setCell curPos 0b00 board)
+        else
+          if oPoss .&. 1 == 1
+            then go (shiftR fPoss 1) (shiftR oPoss 1) (curPos + 1) (setCell curPos 0b01 board)
+            else go (shiftR fPoss 1) (shiftR oPoss 1) (curPos + 1) (setCell curPos 0b10 board)
+
 boards :: [Board]
 boards = do
   (used, b) <- placeFs positions 0
@@ -69,32 +93,40 @@ boards = do
   let positions'' = filter (not . (`elem` used)) positions'
   placeXs positions'' b
 
-foxPositions = forward ++ (reverse <$> forward) where
-  forward =
-    [[0, 1, 2],
-      [1, 2, 3],
-      [4, 5, 6],
-      [5, 6, 7],
-      [8, 9, 10],
-      [9, 10, 11],
-      [12, 13, 14],
-      [13, 14, 15],
-      [0, 5, 10],
-      [5, 10, 15],
-      [1, 6, 11],
-      [4, 9, 14],
-      [0, 4, 8],
-      [4, 8, 12],
-      [1, 5, 9],
-      [5, 9, 13],
-      [2, 6, 10],
-      [6, 10, 14],
-      [3, 7, 11],
-      [7, 11, 15],
-      [2, 5, 8],
-      [3, 6, 9],
-      [6, 9, 12],
-      [7, 10, 13]]
+boardsGray :: [Board]
+boardsGray = do
+  fPoss <- grayCombos 16 fs
+  oPoss <- grayCombos (16 - fs) os
+  pure (placeAll fPoss oPoss 0)
+
+foxPositions = forward ++ (reverse <$> forward)
+  where
+    forward =
+      [ [0, 1, 2],
+        [1, 2, 3],
+        [4, 5, 6],
+        [5, 6, 7],
+        [8, 9, 10],
+        [9, 10, 11],
+        [12, 13, 14],
+        [13, 14, 15],
+        [0, 5, 10],
+        [5, 10, 15],
+        [1, 6, 11],
+        [4, 9, 14],
+        [0, 4, 8],
+        [4, 8, 12],
+        [1, 5, 9],
+        [5, 9, 13],
+        [2, 6, 10],
+        [6, 10, 14],
+        [3, 7, 11],
+        [7, 11, 15],
+        [2, 5, 8],
+        [3, 6, 9],
+        [6, 9, 12],
+        [7, 10, 13]
+      ]
 
 hasFox board = getAny $ foldMap (\ps -> Any $ cell (ps !! 0) board == 0b00 && cell (ps !! 1) board == 0b01 && cell (ps !! 2) board == 0b10) foxPositions
 
@@ -112,7 +144,10 @@ main = do
   print (combos 1 [1 .. 16])
   print (combos 2 [1 .. 16])
   print (length (combos fs [1 .. 16]) * length (combos os [1 .. 16 - fs]))
-  let wins = filter (not . hasFox) boards
+  for (grayCombos 6 3) (putStrLn . flip (showIntAtBase 2 intToDigit) "")
+  -- let wins = filter (not . hasFox) boards
+  let wins = filter (not . hasFox) boardsGray
   print (length wins)
+
   for wins $ \b -> do
     putStrLn (showBoard b ++ "\n")
